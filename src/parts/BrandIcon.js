13bf5567c5d7fce03d309b94a2336435b6a43186
cleversamerr <thedev.samer@gmail.com>
2023-04-10 18:31:36 +0300

    updated brand name

--- src/parts/BrandIcon.js
+++ src/parts/BrandIcon.js
@@ -3,9 +3,9 @@ import Button from "elements/Button";
 const BrandIcon = () => {
   return (
     <Button className="" type="link" href="/">
-      <p className="text-theme-blue text-4xl">
-        Racx
-        <span className="text-theme-purple">studio</span>
+      <p className="text-theme-blue text-4xl font-bold">
+        <span className="text-theme-purple">thirty</span>
+        digits
       </p>
     </Button>
   );
